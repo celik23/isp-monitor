79163642be3cc7c6f59b46eece93d587f9cc9311
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-01 18:49:19 -0600

    Create venv and account for visualize service

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-apt install speedtest
+apt install speedtest python3-venv -y
+
+python3 -m venv /srv/isp-monitor/venv
+/srv/isp-monitor/venv/bin/python3 -m pip install pip setuptools wheel --upgrade --no-cache-dir
+/srv/isp-monitor/venv/bin/python3 -m pip install -r /srv/isp-monitor/requirements.txt --upgrade --no-cache-dir
 
 cp /srv/isp-monitor/speedtest.service /etc/systemd/system/
 cp /srv/isp-monitor/speedtest.timer /etc/systemd/system/
+
+cp /srv/isp-monitor/visualize.service /etc/systemd/system/
+cp /srv/isp-monitor/visualize.timer /etc/systemd/system/
+
+mkdir /srv/isp-monitor/plots -p
+
 systemctl daemon-reload
 systemctl enable --now speedtest.timer
+systemctl enable --now visualize.timer
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-sudo systemctl stop speedtest.timer
-sudo systemctl stop speedtest.service
-sudo systemctl disable speedtest.timer
-sudo systemctl disable speedtest.service
+systemctl stop speedtest.timer
+systemctl stop speedtest.service
+systemctl disable speedtest.timer
+systemctl disable speedtest.service
 
-sudo rm /etc/systemd/system/speedtest.timer
-sudo rm /etc/systemd/system/speedtest.service
+rm /etc/systemd/system/speedtest.timer
+rm /etc/systemd/system/speedtest.service
 
-sudo systemctl daemon-reload
+systemctl stop visualize.timer
+systemctl stop visualize.service
+systemctl disable visualize.timer
+systemctl disable visualize.service
+
+rm /etc/systemd/system/visualize.timer
+rm /etc/systemd/system/visualize.service
+
+systemctl daemon-reload
